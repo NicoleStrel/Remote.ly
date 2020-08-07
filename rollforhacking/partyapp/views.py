08012ebd1d6115046c80cd23f37bc5ccf759f86a
Nicole Streltsov <streltsovnicole@gmail.com>
2020-08-07 14:55:40 -0400

    simple views

--- rollforhacking/partyapp/views.py
+++ rollforhacking/partyapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
 
 # Create your views here.
+def index(request):
+
+    return render(request, "partyapp/index.html", {
+        "page_title": "Enginering Stores",
+        "description": "Play games with your friends, wherever you are!"
+    })
+
+def fishgame(request):
+
+    return render(request, "partyapp/games/fishgame.html", {
+        "page_title": "Fish Game",
+        "description": "Catch the most Fish!",
+    })
