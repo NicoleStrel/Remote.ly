733ccc5adc3672eb2f290e5beebc255cc5675d68
Nicole Streltsov <streltsovnicole@gmail.com>
2020-08-08 12:12:35 -0400

    for heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn rollforhacking.wsgi --log-file -
--- rollforhacking/rollforhacking/settings.py
+++ rollforhacking/rollforhacking/settings.py
@@ -140,7 +140,9 @@ SITE_ID = 1
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
