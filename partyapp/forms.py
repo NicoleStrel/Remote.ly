1a4485d011a6a597e8b4c43da1b909147496dde1
Arsh Noor Amin <arshnoor@diginera.com>
2020-08-08 17:17:34 -0500

    some clean up

--- partyapp/forms.py
+++ partyapp/forms.py
@@ -10,11 +10,3 @@ class SignupForm(forms.Form):
         profile.lng = self.cleaned_data['lng']
         user.save()
         profile.save()
-# class SignupForm(forms.Form):
-#     lat = forms.CharField(max_length=30, label='Voornaam')
-#     last_name = forms.CharField(max_length=30, label='Achternaam')
-#
-#     def signup(self, request, user):
-#         user.first_name = self.cleaned_data['first_name']
-#         user.last_name = self.cleaned_data['last_name']
-#         user.save()
